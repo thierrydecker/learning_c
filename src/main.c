d1e308afe5200c3bdc128a65632f04543e9b38f2
tdecker <td380th86>
2019-09-24 15:47:44 +0200

    Initial commit

--- src/main.c
+++ src/main.c
@@ -7,8 +7,8 @@
 int main (int argv, char *args[])
 {
 
-        int a = 124;
-        int b = 32;
+        int a = 40;
+        int b = 12;
         printf ("GCD of %d and %d is %d", a, b, gcd (a, b));
 
         return EXIT_SUCCESS;
