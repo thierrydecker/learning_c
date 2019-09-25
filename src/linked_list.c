6cd1eddafd4895a92f5dc44bb51e040b756f7225
tdecker <td380th86>
2019-09-25 20:14:21 +0200

    Initial commit

--- src/linked_list.c
+++ src/linked_list.c
@@ -1,6 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "linked_list.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ * Call functions
+ * */
+void linked_list (void)
+{
+        struct Linked_List_Node *head = NULL;
+        struct Linked_List_Node *second = NULL;
+        struct Linked_List_Node *third = NULL;
+
+        head = (struct Linked_List_Node *) malloc (sizeof (struct Linked_List_Node));
+        second = (struct Linked_List_Node *) malloc (sizeof (struct Linked_List_Node));
+        third = (struct Linked_List_Node *) malloc (sizeof (struct Linked_List_Node));
+
+        head->data = 100;
+        head->next = second;
+
+        second->data = 200;
+        second->next = third;
+
+        third->data = 300;
+        third->next = NULL;
+
+        print_list (head);
+}
 
 /*
  * Print any given list
--- src/main.c
+++ src/main.c
@@ -8,24 +8,8 @@
 int main (int argv, char *args[])
 {
 
-        struct Linked_List_Node *head = NULL;
-        struct Linked_List_Node *second = NULL;
-        struct Linked_List_Node *third = NULL;
 
-        head = (struct Linked_List_Node *) malloc (sizeof (struct Linked_List_Node));
-        second = (struct Linked_List_Node *) malloc (sizeof (struct Linked_List_Node));
-        third = (struct Linked_List_Node *) malloc (sizeof (struct Linked_List_Node));
-
-        head->data = 100;
-        head->next = second;
-
-        second->data = 200;
-        second->next = third;
-
-        third->data = 300;
-        third->next = NULL;
-
-        print_list (head);
+        linked_list ();
 
         return EXIT_SUCCESS;
 
