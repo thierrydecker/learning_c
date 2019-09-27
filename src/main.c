94b002e4e7bdfb82d7fcfea83f21302c101a31d9
tdecker <td380th86>
2019-09-27 07:32:34 +0200

    Initial commit

--- src/main.c
+++ src/main.c
@@ -1,23 +1,9 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include "processes.h"
 
 /*
  * Main function
  * */
 int main (int argv, char *args[])
 {
-        pid_t forked_pid = fork ();;
-
-        if (forked_pid > 0) {
-                printf ("I am the parent!\n");
-        } else if (forked_pid == 0) {
-                printf ("I am the child!\n");
-        } else if (forked_pid < 0) {
-                perror ("fork");
-                return EXIT_FAILURE;
-        }
-        process_print ();
         return EXIT_SUCCESS;
 }
